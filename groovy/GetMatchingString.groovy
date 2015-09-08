47c5f621d1ae47fad22bd5a67cd5c0e80df72fe2
nmrao <nagamalli.s@gmail.com>
2015-09-08 10:39:46 +0530

    Create GetMatchingString.groovy

---
+++ groovy/GetMatchingString.groovy
@@ -0,0 +1,17 @@
+/**This script extracts a partial string of a soap response node value of matching string 
+**and assigns a test case property
+**/
+def xml = new XmlHolder(context.response)
+//change the xpath as per the need
+def responseValue = xml.getNodeValue("//*:CDES-PUT-Response/*:response-message")
+//partial string of a node value from the responseValue
+def string = 'Job received successfully'
+// partial string of the same node value from the responseValue to be matched and extracted
+// using regex
+def pattern = '[A-Z]+[0-9]+'
+if (responseValue.contains(string)) {
+    def group = (responseValue =~ /${pattern}/)
+    context.testCase.setPropertyValue('JOB_ID',group[0])
+} else {
+	log.warning "Unexpected value in the response"
+}
