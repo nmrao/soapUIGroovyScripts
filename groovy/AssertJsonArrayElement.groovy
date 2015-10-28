bcf2b215f7d5d09d229a0f343b50d474633c5a6d
nmrao <nagamalli.s@gmail.com>
2015-10-28 16:49:19 +0530

    Create AssertJsonArrayElement.groovy

---
+++ groovy/AssertJsonArrayElement.groovy
@@ -0,0 +1,28 @@
+/**
+* this example shows how to assert a json element of an array
+**/
+def jsonText = '''{
+  "cars": [
+    {
+      "name" : "Jetta",
+      "madeBy" : "Volkswagen"
+    },
+    {
+      "name" : "Polo GT",
+      "madeBy" : "Volkswagen"
+    },
+    {
+      "name" : "i30",
+      "madeBy" : "Hyundai"
+    }
+  ]
+}'''
+def jsonSlurper = new JsonSlurper()
+def cars = jsonSlurper.parseText(jsonText).cars
+def expectedName = 'Jetta'
+def expectedValueExists=false
+if (cars) {
+    cars.each{if (it.name==expectedName) {expectedValueExists=true} }
+}
+if (expectedValueExists) { println "Expected car ${expectedName} exists"}
+assert expectedValueExists, "Expected car name does not exists"
