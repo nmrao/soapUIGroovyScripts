4b0ac22b73ab460ea504502442616c0d5df866a3
nmrao <nagamalli.s@gmail.com>
2017-06-20 14:29:38 +0530

    Update RunDifferentTestSuite.groovy

--- groovy/RunDifferentTestSuite.groovy
+++ groovy/RunDifferentTestSuite.groovy
@@ -2,7 +2,8 @@
 * Refer: https://community.smartbear.com/t5/SoapUI-NG/How-to-run-a-specific-testSuite-using-groovy-script/m-p/144456/highlight/false#M32686
 *
 **/
-
+import com.eviware.soapui.support.types.StringToObjectMap
+//Replace the suite name in below statement.
 def suiteNameToExecute = 'TestSuite1'
 
 def runSuite = { suiteName, async = true ->
