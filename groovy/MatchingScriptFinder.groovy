5d5ca51e58c8d45386d41b26d06b4f8c796c1610
nmrao <nagamalli.s@gmail.com>
2017-08-22 12:06:28 +0530

    Create MatchingScriptFinder.groovy

---
+++ groovy/MatchingScriptFinder.groovy
@@ -0,0 +1,38 @@
+/**
+ * This groovy script is to find 
+ * the details of groovy script test step 
+ * which has certain string defined by the user
+ * in the variable "key"
+ * 
+ * Ref: https://community.smartbear.com/t5/SoapUI-Open-Source/how-to-fetch-all-the-TC-name-and-test-step-names-that-have-db/m-p/148309#M24957
+ **/
+import com.eviware.soapui.impl.wsdl.teststeps.WsdlGroovyScriptTestStep
+//Define the string to be searched for in a script
+def key = 'ORDERID'
+
+/**
+ * Don't edit beyond this point
+ **/
+def stepTypes = [WsdlGroovyScriptTestStep]
+def project = context.testCase.testSuite.project
+def currentStepMap = [
+	suite : context.testCase.testSuite.name, 
+	kase  : context.testCase.name,
+	step  : context.currentStep.name
+]
+def logMatchingScript = { suite, kase, step ->  
+	def tempMap = [suite : suite.name, kase : kase.name, step: step.name]	
+	def result = currentStepMap != tempMap ? true : false
+	if (result &&(stepTypes.any{step in it}) && (step?.script?.contains(key)) ) {
+		log.info "Matching details: ${suite.name} - ${kase.name} - ${step.name}"
+	}
+}
+
+project.testSuiteList.each { suite ->
+	suite.testCaseList.each { kase ->
+		kase.testStepList.each { step ->
+			logMatchingScript(suite, kase, step)
+		}
+	}	
+}
+log.info 'done'
