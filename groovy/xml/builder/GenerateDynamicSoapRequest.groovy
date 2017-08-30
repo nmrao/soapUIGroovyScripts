7eeed5bb811d0f73a89ca8001c6d0c80878b2a8b
nmrao <nagamalli.s@gmail.com>
2017-08-30 09:37:27 +0530

    Update GenerateDynamicSoapRequest.groovy

--- groovy/xml/builder/GenerateDynamicSoapRequest.groovy
+++ groovy/xml/builder/GenerateDynamicSoapRequest.groovy
@@ -1,5 +1,5 @@
 /**
- * Below script shows how to build xml using MarkupBuiler with easily 
+ * Below script shows how to build xml using MarkupBuiler with easily
  * using element names as need by the user
  * just like how xml appears
  * (as shown in color code;elements, data)
@@ -10,13 +10,13 @@ import groovy.xml.XmlUtil
 
 //Define all your namespaces here
 def nameSpacesMap = [
- soap: 'http://schemas.xmlsoap.org/soap/envelope/',
- ns: 'http://www.domain1.example.com/person',
- ns1: 'http://www.domain2.example.com/student',
- xsi: 'http://www.w3.org/2001/XMLSchema-instance'
+        soap: 'http://schemas.xmlsoap.org/soap/envelope/',
+        ns: 'http://www.domain1.example.com/person',
+        ns1: 'http://www.domain2.example.com/student',
+        xsi: 'http://www.w3.org/2001/XMLSchema-instance'
 ]
 
-//For instance, let us assume the data read[record] 
+//For instance, let us assume the data read[record]
 //from datasource
 def name = 'John'
 def age = 20
@@ -25,28 +25,28 @@ def education
 def adress
 
 def createRequest = {
- def builder = new StreamingMarkupBuilder()
- builder.encoding = 'utf-8'
- def soapRequest = builder.bind {
-  mkp.xmlDeclaration()
-  namespaces << nameSpacesMap
-  soap.Envelope {
-   soap.Body {
-    //example for element attribute
-    ns.UserRequest(type: type) {
-     ns1.name(name)
-     ns1.age(age)
-      //add education element irrespective of data
-     ns1.education(education)
-      //add address only if there is data
-     if (!"$address") {
-      ns1.address(address)
-     }
+    def builder = new StreamingMarkupBuilder()
+    builder.encoding = 'utf-8'
+    def soapRequest = builder.bind {
+        mkp.xmlDeclaration()
+        namespaces << nameSpacesMap
+        soap.Envelope {
+            soap.Body {
+                //example for element attribute
+                ns.UserRequest(type: type) {
+                    ns1.name(name)
+                    ns1.age(age)
+                    //add education element irrespective of data
+                    ns1.education(education)
+                    //add address only if there is data
+                    if (!"$address") {
+                        ns1.address(address)
+                    }
+                }
+            }
+        }
     }
-   }
-  }
- }
- XmlUtil.serialize(soapRequest)
+    XmlUtil.serialize(soapRequest)
 }
 
 println createRequest()
