00fb4ee06e6c1c7926671126803cbbb01d1c2a3a
nmrao <nagamalli.s@gmail.com>
2017-09-18 12:43:46 +0530

    Create QueryRelativeData.groovy

---
+++ groovy/xml/QueryRelativeData.groovy
@@ -0,0 +1,12 @@
+/**
+* Ref: https://community.smartbear.com/t5/SoapUI-Pro/Need-help-with-XmlSlurper/m-p/149851#M34183
+* Online demo: https://ideone.com/z8Rg76
+* Below is the script assertion
+**/
+assert context.response, 'Response is empty or null'
+def xml = new XmlSlurper().parseText(context.response)
+def docs = xml.'**'.findAll{it.name() == 'Document'}
+def map = (docs.collect{it.Name.@id.text()} as Set).collectEntries{id -> [(id as Integer): docs.findAll{it.Name.@id.text() == id && it.DateOfIssuance.@value.text() && it.DateOfIssuance.@value.text() != "-" }.collect{Date.parse('dd.MM.yyyy', it.DateOfIssuance.@value.text())}.max()]}.sort{it.key}
+log.info map
+
+​
