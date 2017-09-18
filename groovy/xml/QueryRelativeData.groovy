98c57069288fda6bd677ff56f8168c8cb329e25e
nmrao <nagamalli.s@gmail.com>
2017-09-18 14:33:11 +0530

    Update QueryRelativeData.groovy

--- groovy/xml/QueryRelativeData.groovy
+++ groovy/xml/QueryRelativeData.groovy
@@ -4,9 +4,20 @@
 * Below is the script assertion
 **/
 assert context.response, 'Response is empty or null'
+def dateFormat = 'dd.MM.yyyy'
 def xml = new XmlSlurper().parseText(context.response)
-def docs = xml.'**'.findAll{it.name() == 'Document'}
-def map = (docs.collect{it.Name.@id.text()} as Set).collectEntries{id -> [(id as Integer): docs.findAll{it.Name.@id.text() == id && it.DateOfIssuance.@value.text() && it.DateOfIssuance.@value.text() != "-" }.collect{Date.parse('dd.MM.yyyy', it.DateOfIssuance.@value.text())}.max()?.format('dd.MM.yyyy')]}.sort{it.key}.findAll{it.value}
-map.collect { log.info "Max date of issuance for ${it.key} is ${it.value}" }
 
-​
+//Get all the documents
+def docs = xml.'**'.findAll{it.name() == 'Document'}
+
+//Closure to get the max date of selected id
+def getMaxDateOfId = { id -> docs.findAll { it.Name.@id.text() == id && it.DateOfIssuance.@value.text() && it.DateOfIssuance.@value.text() != "-" }
+      .collect { Date.parse(dateFormat, it.DateOfIssuance.@value.text()) }
+      .max()?.format(dateFormat) 
+}
+
+def map = (docs.collect{it.Name.@id.text()} as Set).collectEntries{ id -> [(id as Integer): getMaxDateOfId(id)] }
+.sort{it.key}
+.findAll{it.value}
+
+map.collect { log.info "Max date of issuance for ${it.key} is ${it.value}" }
