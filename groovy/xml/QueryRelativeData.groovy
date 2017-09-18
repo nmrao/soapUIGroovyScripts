64dadd94baa6db8777755dfa23db752dd49f14bb
nmrao <nagamalli.s@gmail.com>
2017-09-18 14:19:05 +0530

    Update QueryRelativeData.groovy

--- groovy/xml/QueryRelativeData.groovy
+++ groovy/xml/QueryRelativeData.groovy
@@ -6,7 +6,7 @@
 assert context.response, 'Response is empty or null'
 def xml = new XmlSlurper().parseText(context.response)
 def docs = xml.'**'.findAll{it.name() == 'Document'}
-def map = (docs.collect{it.Name.@id.text()} as Set).collectEntries{id -> [(id as Integer): docs.findAll{it.Name.@id.text() == id && it.DateOfIssuance.@value.text() && it.DateOfIssuance.@value.text() != "-" }.collect{Date.parse('dd.MM.yyyy', it.DateOfIssuance.@value.text())}.max()]}.sort{it.key}
-log.info map
+def map = (docs.collect{it.Name.@id.text()} as Set).collectEntries{id -> [(id as Integer): docs.findAll{it.Name.@id.text() == id && it.DateOfIssuance.@value.text() && it.DateOfIssuance.@value.text() != "-" }.collect{Date.parse('dd.MM.yyyy', it.DateOfIssuance.@value.text())}.max()?.format('dd.MM.yyyy')]}.sort{it.key}.findAll{it.value}
+map.collect { log.info "Max date of issuance for ${it.key} is ${it.value}" }
 
 ​
