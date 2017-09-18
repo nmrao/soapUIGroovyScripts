a2035b71a7c1b5ab047ac565e58e2075ad7843b5
nmrao <nagamalli.s@gmail.com>
2017-09-18 08:12:26 +0530

    Create XmlWithArrayOfElements.groovy

---
+++ groovy/xml/XmlWithArrayOfElements.groovy
@@ -0,0 +1,8 @@
+/**
+* Ref: https://community.smartbear.com/t5/SoapUI-Pro/How-to-parse-JSON-or-XML-in-Assertions/m-p/149830#M34177
+* This is script assertion for reading json response and get the size of the array and print array data
+* Demo : https://ideone.com/FGSawS
+def xml = new XmlSlurper().parseText(context.response)
+def members = xml.'**'.findAll{it.name() == 'e'}
+log.info members.size()
+log.info members.collect{it.MemberName}
