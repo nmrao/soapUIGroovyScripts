f4c837548bb5a48d3160c2c1dfc5f71bdc286422
nmrao <nagamalli.s@gmail.com>
2019-01-23 11:52:26 +0530

    Create CreatePropertiesFromJdbcResult.groovy

---
+++ groovy/CreatePropertiesFromJdbcResult.groovy
@@ -0,0 +1,28 @@
+/**
+* Below is the Script Assertion for the JDBC Request test step
+* extracts column names and values for a single row (assuming query retuns a single row)
+* and put the extracted data into following Properties test step
+* Refer for more details
+*
+** /
+
+//For testing, using fixed response
+//Comment it and uncomment  line 19 when you need the script to process current jdbc test step response
+def jdbcResponse = """<Results>
+   <ResultSet fetchSize="0">
+      <Row rowNumber="1">
+         <CATEGORY.CAT_ID>7</CATEGORY.CAT_ID>
+         <CATEGORY.CAT_NAME>Human Resource</CATEGORY.CAT_NAME>
+      </Row>
+      </ResultSet>
+</Results>"""
+//def jdbcResponse = context.response
+
+def xml = new XmlSlurper().parseText(jdbcResponse)
+def step = context.testCase.testSteps['Properties']
+def map = [:]
+xml.'**'.find{it.name() == 'Row'}.childNodes().each { 
+	def prop = step.hasProperty(it.name()) ? step.getProperty(it.name()) : step.addProperty(it.name())
+	prop.value = it.text()
+}
+log.info "Check the properties step for the details"
