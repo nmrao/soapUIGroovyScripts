10ae1f089e71b3db86090228c12a494c5624d44b
nmrao <nagamalli.s@gmail.com>
2019-02-13 08:36:34 +0530

    Create SetJMSProperties.groovy

---
+++ groovy/jms/SetJMSProperties.groovy
@@ -0,0 +1,30 @@
+/**
+* Below is the Groovy Script
+* which can add the JMS Properties to the given test step from a map
+*
+* Ref:
+**/
+
+import com.eviware.soapui.config.JMSPropertyConfig
+
+//Properties to be set to JMS
+def map = [a:'one', b: 'two']
+
+//Provide the step name to set the properties
+def stepName = 'Submit Order_OrderSubmission'
+
+//Donot modify beyond this
+def step = context.testCase.testSteps[stepName]
+def existingProperties =  step.httpRequest.getJMSPropertiesConfig().getJMSProperties()
+
+def newList = new ArrayList<JMSPropertyConfig>();
+map.each { key, value ->
+	def jmsPropertyConfig = JMSPropertyConfig.Factory.newInstance()
+	jmsPropertyConfig.setName(key)
+	jmsPropertyConfig.setValue(value)
+	newList.add(jmsPropertyConfig)
+}
+
+            
+existingProperties.clear()
+step.httpRequest.getJMSPropertiesConfig().setJMSProperties(newList)
