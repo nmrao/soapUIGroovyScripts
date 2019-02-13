5359b25968212a5a2e3add6718aab0237b02f3cf
nmrao <nagamalli.s@gmail.com>
2019-02-13 20:11:37 +0530

    Create VaryingQueryAndHeadersToRestService.groovy

---
+++ groovy/rest/VaryingQueryAndHeadersToRestService.groovy
@@ -0,0 +1,32 @@
+/**
+* Groovy script to call varying query and header parameters
+* parameters are read from custom properties of test case
+*
+* Ref:
+***/
+
+import wslite.rest.*
+
+def getMap = { key ->
+	def props = context.testCase.propertyNames.findAll { it.startsWith(key)}
+	def result = [:]
+	props.each { result [it.split('_').last()] = context.testCase.getPropertyValue(it) }
+	result
+}
+
+
+def headerz = getMap('HEADER')
+def queriez = getMap('QUERY')
+log.info headers
+log.info queries
+def serviceHost = context.expand('${#Project#SERVICE_HOST_PORT}'
+def urlPath = '/agents/organizations'
+
+def client = new RESTClient(serviceHost)
+def response = client.get(path: urlPath,
+					 accept: ContentType.JSON,
+					 query : queriez,
+					 headers: headerz
+					 )
+assert response.statusCode == 200
+log.info groovy.json.JsonOutput.prettyPrint(response.text)
