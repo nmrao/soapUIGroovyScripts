51a1fe3c24bf989ebd7b3f86881f5b92d4a770fb
nmrao <nagamalli.s@gmail.com>
2021-07-29 20:10:28 +0530

    Update CreateSubProject.groovy

--- groovy/CreateSubProject.groovy
+++ groovy/CreateSubProject.groovy
@@ -6,6 +6,8 @@ And writes the sub set of the project (as all the suites are removed) into given
 Context: If the project size is very big, it takes time to load the project into the tool.
 This script will help just to only create small project out of the big one, so that load the project with given suite in quick time.
 
+Ref: https://community.smartbear.com/t5/API-Functional-Security-Testing/Best-way-to-load-test-suite-as-per-test-requirement/m-p/220444#M49415
+
 How to execute this script?
 You may create an empty project and create an arbitary test case and add groovy script test step and paste the below script in there
 and modify values for below
